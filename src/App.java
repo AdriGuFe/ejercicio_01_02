8d08d5bf9654ed2c9ef454d1151f1991e2c8d635
Adrian Gutierrez <capitanadriang@gmail.com>
2025-09-25 09:53:01 +0200

    added information

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Adrián Gutiérrez Fernández");
+        System.out.println("Calle Cristobal Toral, 15 -Cártama (Málaga) - España");
+        System.out.println("695 04 47 43");
     }
 }
